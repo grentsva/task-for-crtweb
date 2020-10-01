03ea389f7152a7236641298cffc252e07fe62b0b
Vladislav Grents <vladair_o@hotmail.com>
2020-10-01 16:18:00 +0500

    Updates

---
+++ precache-manifest.3beb870671ec5bf6f7d9f8d532a0c3de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1d86c0e649876d4d990f8f866b6559",
+    "url": "/task-for-crtweb/index.html"
+  },
+  {
+    "revision": "3dd35eb628b0a1f62d95",
+    "url": "/task-for-crtweb/static/css/main.b2801373.chunk.css"
+  },
+  {
+    "revision": "6036d74b9f6be757404a",
+    "url": "/task-for-crtweb/static/js/2.522fee1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-for-crtweb/static/js/2.522fee1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd35eb628b0a1f62d95",
+    "url": "/task-for-crtweb/static/js/main.d3306192.chunk.js"
+  },
+  {
+    "revision": "44db5dad247ae77b5d7a",
+    "url": "/task-for-crtweb/static/js/runtime-main.c3d3e794.js"
+  },
+  {
+    "revision": "6e2fe115f44f89d49c04293020fe0b4b",
+    "url": "/task-for-crtweb/static/media/preloader.6e2fe115.svg"
+  },
+  {
+    "revision": "413dd68bccc5bfb212a5d711ab132984",
+    "url": "/task-for-crtweb/static/media/unlike.413dd68b.png"
+  }
+]);
